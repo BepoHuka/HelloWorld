5ca94b2de38974871dfb2ad126ac2b7680163992
veronika.pasechnik <veronika.pasechnik>
2016-03-04 15:47:25 +0300

    Ins sorting type has been added

--- ArraySorting.java
+++ ArraySorting.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 import java.util.Random;
  
-public class ArraySorting {
+public class ArraySorting 
+{
 
     public static void main(String[] args)
     {
@@ -9,27 +10,37 @@ public class ArraySorting {
         System.out.print("¬ведите размер массива: ");
         int arraySize = in.nextInt();		//переменна€ хран€ща€ размер массива (введЄнный с консоли)
         int [] array = new int[arraySize];	//переменна€ хран€ща€ массив
+        int [] array2 = new int[arraySize];
         array = randomize(arraySize);
+        array2 = randomize(arraySize);
         
         System.out.print("—генерирован массив случайных чисел: "); 
-        for (int i = 0; i < arraySize; i++) {
+        for (int i = 0; i < arraySize; i++) 
+        {
         	System.out.print(array[i] + " ");
         }
-        System.out.println();
+        	System.out.println();
+           	array = bublesort(array);
+           	array2 = insertionSort(array);
         
-        array = sort(array);
-        
-        System.out.print("ћассив отсортирован по возрастанию: ");         
-        for (int i = 0; i < arraySize; i++) {
-        	System.out.print(array[i] + " ");
-        }
-        System.out.println();
-        
+           	System.out.print("ћассив отсортирован по возрастанию методом пузырька: ");         
+           	for (int i = 0; i < arraySize; i++) 
+           		{
+           			System.out.print(array[i] + " ");
+           		}
+           	System.out.println();
+           	System.out.print("ћассив отсортирован по возрастанию методом вставки: ");         
+           	for (int i = 0; i < arraySize; i++) 
+           		{
+           			System.out.print(array2[i] + " ");
+           		}
+       	System.out.println();
         in.close();
         
     }
     
-    private static int[] randomize(int size) {
+    private static int[] randomize(int size) 
+    {
         int [] randomArray = new int[size];
 
         Random randomushka = new Random();  
@@ -43,11 +54,15 @@ public class ArraySorting {
             
     }
     
-    private static int[] sort(int[] arrayToSort) {  
+    private static int[] bublesort(int[] arrayToSort) 
+    {  
     	   	
-    	for (int i = 0; i < arrayToSort.length - 1; i++) {
-    		for (int j = 0; j < arrayToSort.length - 1; j++) {
-    			if (arrayToSort[j] > arrayToSort[j + 1]){
+    	for (int i = 0; i < arrayToSort.length - 1; i++) 
+    	{
+    		for (int j = 0; j < arrayToSort.length - 1; j++) 
+    		{
+    			if (arrayToSort[j] > arrayToSort[j + 1])
+    			{
     				int tmp = arrayToSort[j];
     				arrayToSort[j] = arrayToSort[j + 1];
     				arrayToSort[j + 1] = tmp;
@@ -56,5 +71,22 @@ public class ArraySorting {
     	}
     	
     	return arrayToSort;    	
+    	
+    }  
+    
+    private static int[] insertionSort(int[] insArray)
+    {
+    	for (int i = 0; i < insArray.length; i++)
+    	{
+    		int arrayElement = insArray[i];
+    		int j = i-1;
+    		while(j >= 0 && insArray[j] > arrayElement)
+    		{
+    			insArray[j] = insArray[i];
+    			j=j-1;
+    			insArray[j+1]=arrayElement;
+    		}
+       	}
+    	return insArray;
     }
 }
